9b7f9e286a0cf1cf09945992f639b99d97860a04
Anniina <anniina.leikas@outlook.com>
2024-10-03 17:46:54 +0300

    Remove files from main

--- First workshopTasks/Greeting.js
+++
@@ -1,8 +0,0 @@
-let greeting = "Hello, World!";
-console.log(greeting);
-let userName = "Anniina";
-function greet(userName) {
-    return "Hello, " + userName + "!";
-}
-let message = greet(userName);
-console.log(message);
